cafeea88d95ec59e7d698f763d4f3046c6c6306f
Will Leinweber <will@bitfission.com>
2014-02-19 17:22:13 -0800

    migration done, add index

--- schema.sql
+++ schema.sql
@@ -3,20 +3,14 @@ create table guests (
   id               uuid          primary key default uuid_generate_v4(),
   guest_name       text          not null        ,
   herokai_name     text          not null        ,
-  visiting_on      timestamptz   not null        ,
   lunch            boolean       default false   ,
   nda              boolean       default false   ,
   notify_hipchat   boolean       default false   ,
   notify_gchat     boolean       default false   ,
   notify_sms       text                          ,
   notes            text                          ,
-  created_at       timestamptz   default now()
+  created_at       timestamptz   default now()   ,
+  visiting_range   daterange     not null
 );
 
-
- begin;
- alter table guests add column visiting_range daterange;
- update guests set visiting_range = daterange(visiting_on::date, visiting_on::date, '[]');
- alter table guests drop column visiting_on ;
- alter table guests alter column visiting_range set not null;
- commit;
+create index visitng_range_gist on guests using gist(visiting_range);
