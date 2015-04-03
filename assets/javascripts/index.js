cafe6e0d83e457e32345a1b39a516c89c2137b88
Will Leinweber <will@bitfission.com>
2015-04-02 17:56:37 -0700

    employee form

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,8 +1,10 @@
 if (window.location.pathname == "/") {
   document.addEventListener('DOMContentLoaded',function() {
     var form = document.getElementById('newguest');
+    var form2 = document.getElementById('newemployee');
     var toSave = ["herokai_name", "notify_hipchat", "notify_gchat"];
 
+    form2.elements["employee_name"].value = localStorage.getItem("herokai_name")
     form.elements["herokai_name"].value = localStorage.getItem("herokai_name")
     form.elements["notify_gchat"].checked   = (localStorage.getItem("notify_gchat")   === "true")
     form.elements["notify_hipchat"].checked = (localStorage.getItem("notify_hipchat") === "true")
@@ -11,6 +13,10 @@ if (window.location.pathname == "/") {
       localStorage.setItem("herokai_name",   form.elements["herokai_name"].value)
       localStorage.setItem("notify_gchat",   form.elements["notify_gchat"].checked)
       localStorage.setItem("notify_hipchat", form.elements["notify_hipchat"].checked)
+    }, false);
+
+    form2.addEventListener("submit", function(e) {
+      localStorage.setItem("herokai_name",   form2.elements["employee_name"].value)
     }, false);
 
     if (addRowButton = document.getElementById('addRow')) {
