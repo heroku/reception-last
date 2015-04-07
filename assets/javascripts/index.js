cafe5aac45f176f65fc74a95377470c29f2a125a
Will Leinweber <will@bitfission.com>
2015-04-06 18:10:08 -0700

    add buttons to the other side

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,10 +1,8 @@
 if (window.location.pathname == "/") {
   document.addEventListener('DOMContentLoaded',function() {
     var form = document.getElementById('newguest');
-    var form2 = document.getElementById('newemployee');
     var toSave = ["herokai_name", "notify_hipchat", "notify_gchat"];
 
-    form2.elements["employee_name"].value = localStorage.getItem("herokai_name")
     form.elements["herokai_name"].value = localStorage.getItem("herokai_name")
     form.elements["notify_gchat"].checked   = (localStorage.getItem("notify_gchat")   === "true")
     form.elements["notify_hipchat"].checked = (localStorage.getItem("notify_hipchat") === "true")
@@ -15,20 +13,20 @@ if (window.location.pathname == "/") {
       localStorage.setItem("notify_hipchat", form.elements["notify_hipchat"].checked)
     }, false);
 
-    form2.addEventListener("submit", function(e) {
-      localStorage.setItem("herokai_name",   form2.elements["employee_name"].value)
-    }, false);
 
-    if (addRowButton = document.getElementById('addRow')) {
-      addRowButton.addEventListener("click", function(e) {
-        addRowButton.insertAdjacentHTML('beforebegin', document.getElementsByClassName('guest-name')[0].outerHTML)
+    var addButtons = document.getElementsByClassName("addGuest")
+    for (var i=0; i < addButtons.length; i++) {
+      addButtons[i].addEventListener("click", function(e) {
+        var newField = this.parentNode.getElementsByClassName('guest-name')[0].outerHTML
+        this.insertAdjacentHTML('beforebegin', newField)
         e.preventDefault();
       }, false);
     }
 
-    if (removeRowButton = document.getElementById('removeRow')) {
-      removeRowButton.addEventListener("click", function(e) {
-        var elems = removeRowButton.parentElement.getElementsByClassName('guest-name')
+    var removeButtons = document.getElementsByClassName("removeGuest")
+    for (var i=0; i < removeButtons.length; i++) {
+      removeButtons[i].addEventListener("click", function(e) {
+        var elems = this.parentElement.getElementsByClassName('guest-name')
         var idx = elems.length - 1
         if (idx > 0) {
           var elem = elems[idx]
